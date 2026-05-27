3eeb5a6e8fa7cd37d9c40a3867be1d4a0d058e4c
Kamal Chandra Upreti <kmlupreti@protonmail.com>
2026-05-27 17:26:10 +0545

    add method to get lexeme of a token

--- src/scanner/token.rs
+++ src/scanner/token.rs
@@ -15,6 +15,9 @@ impl Token {
             line,
         }
     }
+    pub fn lexeme(&self) -> String {
+        self.lexeme.clone()
+    }
 }
 impl Display for Token {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
