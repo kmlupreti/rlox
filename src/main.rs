39fcb7790468789c80207cbc65ebb766eb3f2839
Kamal Chandra Upreti <kmlupreti@protonmail.com>
2026-06-02 11:17:22 +0545

    handle errors properly

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,15 @@
 use std::env::args;
 use std::process::exit;
 
-fn main() {
+fn main() -> Result<(), ()> {
     let mut args = args().skip(1);
     if args.len() == 1 {
-        lox::run_file(args.nth(0).unwrap())
+        lox::run_file(args.nth(0).unwrap())?
     } else if args.len() > 1 {
         eprintln!("usage: lox <script>");
         exit(64)
     } else {
-        lox::run_prompt()
+        lox::run_prompt()?
     }
+    Ok(())
 }
