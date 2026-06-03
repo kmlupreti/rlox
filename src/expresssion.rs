8172a061c6c689a14465e040ec45ef2e98ae512e
Kamal Chandra Upreti <kmlupreti@protonmail.com>
2026-06-03 19:10:02 +0545

    Add method to evalute expressions

--- src/expresssion.rs
+++ src/expresssion.rs
@@ -1,4 +1,4 @@
-use crate::token::Token;
+use crate::{lox_value::LoxValue, token::Token, token_type::TokenType};
 
 #[derive(Debug)]
 pub enum Expr {
@@ -40,6 +40,77 @@ impl Expr {
             }
             Self::Grouping { expr } => {
                 format!("(group {})", expr.print_ast())
+            }
+        }
+    }
+    pub fn evaluate(&self) -> LoxValue {
+        match &self {
+            Self::Literal { value } => match value.token_type {
+                TokenType::String => {
+                    LoxValue::String((value.lexeme[1..value.lexeme.len() - 1]).to_string())
+                }
+                TokenType::Number => LoxValue::Number(value.lexeme.parse().unwrap()),
+                TokenType::False => LoxValue::Boolean(false),
+                TokenType::True => LoxValue::Boolean(true),
+                _ => LoxValue::Null,
+            },
+            Self::Unary { operator, right } => {
+                let right = right.evaluate();
+                match operator.token_type {
+                    TokenType::Minus => match right {
+                        LoxValue::Number(n) => LoxValue::Number(-n),
+                        LoxValue::String(s) => LoxValue::Number(-s.parse::<f64>().unwrap()),
+                        _ => LoxValue::Null,
+                    },
+                    TokenType::Bang => match right {
+                        LoxValue::Boolean(b) => LoxValue::Boolean(!b),
+                        _ => LoxValue::Boolean(true),
+                    },
+                    _ => LoxValue::Null,
+                }
+            }
+            Self::Grouping { expr } => expr.evaluate(),
+            Self::Binary {
+                left,
+                operator,
+                right,
+            } => {
+                let left = left.evaluate();
+                let right = right.evaluate();
+                match operator.token_type {
+                    TokenType::Minus => {
+                        if let LoxValue::Number(n1) = left
+                            && let LoxValue::Number(n2) = right
+                        {
+                            LoxValue::Number(n1 - n2)
+                        } else {
+                            LoxValue::Null
+                        }
+                    }
+                    TokenType::Star => {
+                        if let LoxValue::Number(n1) = left
+                            && let LoxValue::Number(n2) = right
+                        {
+                            LoxValue::Number(n1 * n2)
+                        } else {
+                            LoxValue::Null
+                        }
+                    }
+                    TokenType::Plus => {
+                        if let LoxValue::Number(n1) = left
+                            && let LoxValue::Number(n2) = right
+                        {
+                            LoxValue::Number(n1 + n2)
+                        } else if let LoxValue::String(s1) = left
+                            && let LoxValue::Number(s2) = right
+                        {
+                            LoxValue::String(format!("{}{}", s1, s2))
+                        } else {
+                            LoxValue::Null
+                        }
+                    }
+                    _ => LoxValue::Null,
+                }
             }
         }
     }
