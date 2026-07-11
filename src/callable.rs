d853968fc03f26d3a0cb26a2709f6b43baf07cc9
Kamal Chandra Upreti <kmlupreti@protonmail.com>
2026-07-11 20:40:40 +0545

    Add method to check if args are valid

--- src/callable.rs
+++ src/callable.rs
@@ -1,4 +1,8 @@
-use crate::{error::LoxError, interpreter::Interpreter, lox_value::LoxValue};
+use crate::{
+    error::{LoxError, LoxResult},
+    interpreter::Interpreter,
+    lox_value::LoxValue,
+};
 
 pub trait Callable {
     fn call(
@@ -8,4 +12,18 @@ pub trait Callable {
         line: usize,
     ) -> Result<LoxValue, LoxError>;
     fn arity(&self) -> usize;
+    fn check_args(&self, args_count: usize, line: usize) -> LoxResult<()> {
+        if args_count >= 255 {
+            return Err(LoxError::CallError {
+                msg: String::from("can't have more than 255 arguments"),
+                line,
+            });
+        } else if self.arity() != args_count {
+            return Err(LoxError::CallError {
+                msg: format!("expected {} arguments but got {}", self.arity(), args_count),
+                line,
+            });
+        }
+        Ok(())
+    }
 }
