e63173d63c9d7392aa9023d3b1cd89a0ed8a2aaf
Kamal Chandra Upreti <kmlupreti@protonmail.com>
2026-07-14 16:17:50 +0545

    Add IO error and make it possible to convert to LoxError, Add error type in error msgs

--- src/error.rs
+++ src/error.rs
@@ -1,5 +1,5 @@
 use crate::{lox_value::LoxValue, token::Token, token_type::TokenType};
-use std::fmt::Display;
+use std::{fmt::Display, io::Error};
 
 pub type LoxResult<T> = Result<T, LoxError>;
 
@@ -14,46 +14,65 @@ pub enum LoxError {
     Break { line: usize },
     ResolveError { line: usize, msg: String },
     GetError { msg: String, line: usize },
+    IOError { msg: String },
 }
 impl Display for LoxError {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         match self {
             LoxError::UnexpectedChar { char, line } => {
-                write!(f, "[line: {}] unexpected character {:?} found", line, char)
+                write!(
+                    f,
+                    "[line: {}] scan error:  unexpected character {:?} found",
+                    line, char
+                )
             }
             LoxError::UnterminatedString { line } => {
-                write!(f, "[line: {}] unterminated string", line)
+                write!(f, "[line: {}] scan error:  unterminated string", line)
             }
             LoxError::ParseError { token, msg } => {
                 if token.token_type == TokenType::Eof {
-                    write!(f, "[line: {}] at end {}", token.line, msg)
+                    write!(f, "[line: {}] parse error: at end {}", token.line, msg)
                 } else {
-                    write!(f, "[line: {}] at '{}' {}", token.line, token.lexeme, msg)
+                    write!(
+                        f,
+                        "[line: {}] at '{}' parse error: {}",
+                        token.line, token.lexeme, msg
+                    )
                 }
             }
             LoxError::RuntimeError { line, msg } => {
-                write!(f, "[line: {}] {}", line, msg)
+                write!(f, "[line: {}] runtime error: {}", line, msg)
             }
             LoxError::CallError { msg, line } => {
-                write!(f, "[line: {}] {}", line, msg)
+                write!(f, "[line: {}] call error: {}", line, msg)
             }
             LoxError::GetError { msg, line } => {
-                write!(f, "[line: {}] {}", line, msg)
+                write!(f, "[line: {}] get error: {}", line, msg)
             }
             LoxError::ResolveError { msg, line } => {
-                write!(f, "[line: {}] {}", line, msg)
+                write!(f, "[line: {}] resolve error: {}", line, msg)
             }
             LoxError::Return { line, value: _ } => {
                 write!(
                     f,
-                    "[line: {}] can't use return outside the function block",
+                    "[line: {}] error: can't use return outside the function block",
                     line
                 )
             }
             LoxError::Break { line } => {
-                write!(f, "[line: {}] can't use break outside loop", line)
+                write!(f, "[line: {}] error: can't use break outside loop", line)
             }
-            _ => write!(f, "unknown error occured"),
+            LoxError::IOError { msg } => {
+                write!(f, "I/O error: {}", msg)
+            }
+            _ => write!(f, "error: unknown error occured"),
+        }
+    }
+}
+impl From<Error> for LoxError {
+    fn from(value: Error) -> Self {
+        Self::IOError {
+            msg: value.to_string(),
         }
     }
 }
