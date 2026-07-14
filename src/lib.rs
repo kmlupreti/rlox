26baec7c5f9262621e38471ce23f0589580f6260
Kamal Chandra Upreti <kmlupreti@protonmail.com>
2026-07-14 17:14:43 +0545

    Revert "Dont lock stdin"
    This reverts commit c232f995a9b68547a9c4c3138a6980dc78b6fd77.

--- src/lib.rs
+++ src/lib.rs
@@ -32,7 +32,7 @@ where
         .interpret(Parser::new(scanner::Scanner::new(&buffer).scan_tokens()?.clone()).parse())
 }
 pub fn run_prompt() -> LoxResult<()> {
-    let stdin = io::stdin();
+    let mut stdin = io::stdin().lock();
     let mut interpreter = Interpreter::new();
     loop {
         print!("> ");
